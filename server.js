fbc77f18ee0c77dfffa250f6d28f3922c9838ce3
Sean <42885396+seanAkhtar@users.noreply.github.com>
2024-06-27 15:08:53 -0500

    model showing todo configure placement

--- server.js
+++ server.js
@@ -4,26 +4,20 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 443; // Default HTTPS port
+const PORT = 443;
 
-// SSL certificate
 const privateKey = fs.readFileSync('server.key', 'utf8');
 const certificate = fs.readFileSync('server.cert', 'utf8');
 const credentials = { key: privateKey, cert: certificate };
 
-// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Test route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Create HTTPS server
 const httpsServer = https.createServer(credentials, app);
 
-// Start the server
 httpsServer.listen(PORT, () => {
   console.log(`HTTPS Server running on port ${PORT}`);
 });
-
